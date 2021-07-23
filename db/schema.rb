2a09d62a283bd2bf56a5d3c00aeae121e8a30685
mayu-tateno <polaris.and.sirius810@i.softbank.jp>
2021-07-23 10:33:05 +0900

    Change tasks name not null

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_21_121558) do
+ActiveRecord::Schema.define(version: 2021_07_23_012751) do
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
